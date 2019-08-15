f2a313c4f1606c7d1f755cdbaa965b1af40e6ca1
pheistman <pheistman@live.co.uk>
2019-08-15 11:49:01 +0100

    Created variables for paths

--- cpbkup.sh
+++ cpbkup.sh
@@ -9,18 +9,22 @@ clear_variables() {
 	count_backup="" 	oldest_backup=""	latest_backup=""
 }
 
+# Location variables
+usb_path='/media/nathan/32gb/timeshift'
+ext_path='/media/nathan/Data/Ubuntu/nathan-backups'
+
 #Execute commands
 clear_variables
-count_backup=`ls -d /media/nathan/Data/Ubuntu/nathan-backups/20* | wc -l`
-latest_backup=`ls /media/nathan/32GB\ Backup/timeshift/snapshots-daily/ | tail -1`
-oldest_backup=`ls /media/nathan/Data/Ubuntu/nathan-backups/ | head -1`
+count_backup=`ls -d $ext_path/20* | wc -l`
+latest_backup=`ls $usb_path/snapshots-daily/ | tail -1`
+oldest_backup=`ls $ext_path/ | head -1`
 if [ "$count_backup" -le "10" ]; then
     less10=1
 else
     less10=0
 fi
 
-if [ -d /media/nathan/Data/Ubuntu/nathan-backups/$latest_backup ]; then
+if [ -d $ext_path/$latest_backup ]; then
    dir_exists=1
 else
     dir_exists=0
@@ -28,20 +32,21 @@ fi
 
 if [ "$less10" -eq "$dir_exists" ]; then
     echo $'\n'$"$(date)  
-The latest backup has already been copied to the destination drive. Exiting..." | tee -a /media/nathan/Data/Ubuntu/nathan-backups/backuplogs.log
+The latest backup has already been copied to the destination drive. Exiting...
+" | tee -a $ext_path/backuplogs.log
     echo ""
 elif [ "$less10" -gt "$dir_exists" ]; then
     echo $'\n'$"$(date)
 +++ Copying latest backup to destination... +++
-" | tee -a /media/nathan/Data/Ubuntu/nathan-backups/backuplogs.log
+" | tee -a $ext_path/backuplogs.log
     echo ""
-    sudo cp -rH /media/nathan/32GB\ Backup/timeshift/snapshots/$latest_backup  /media/nathan/Data/Ubuntu/nathan-backups/
-    sudo mv /media/nathan/Data/Ubuntu/nathan-backups/$oldest_backup /media/nathan/Data/Ubuntu/nathan-backups/delete_me #rename directory
+    sudo cp -rH $usb_path/snapshots/$latest_backup $ext_path 
+    sudo mv $ext_path/$oldest_backup $ext_path/delete_me #rename directory
 else
     echo $'\n'$"$(date)
 +++ Renaming/Deleting oldest backup... +++
-" | tee -a /media/nathan/Data/Ubuntu/nathan-backups/backuplogs.log
+" | tee -a $ext_path/backuplogs.log
     echo ""
-#    sudo cp -rH /media/nathan/32GB\ Backup/timeshift/snapshots/$latest_backup  /media/nathan/Data/Ubuntu/nathan-backups/
-    sudo mv /media/nathan/Data/Ubuntu/nathan-backups/$oldest_backup /media/nathan/Data/Ubuntu/nathan-backups/delete_me #rename directory
+#    sudo cp -rH $usb_path/snapshots/$latest_backup $ext_path 
+    sudo mv $ext_path/$oldest_backup $ext_path/delete_me #rename directory
 fi
